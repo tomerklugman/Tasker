5d9e6fbac9e17b81996a25b783aa8f2c109429f6
tomerklugman <tomerklu@gmail.com>
2022-12-20 04:45:47 +0200

    signin+signup ready

---
+++ app/src/main/java/com/example/tasker/InfoPage.java
@@ -0,0 +1,112 @@
+package com.example.tasker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class InfoPage extends AppCompatActivity {
+
+    TextView name,mail,pass,user;
+    Button logout;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.info_page);
+        name=findViewById(R.id.name);
+        mail=findViewById(R.id.mail);
+        pass=findViewById(R.id.password);
+        user=findViewById(R.id.username);
+        logout=findViewById(R.id.logout);
+
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc= GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount account=GoogleSignIn.getLastSignedInAccount(this);
+        if (account!=null){
+
+            database = FirebaseDatabase.getInstance();
+            reference = database.getReference("users");
+
+            String Name=account.getDisplayName();
+            String Mail=account.getEmail();
+
+
+            name.setText(Name);
+            mail.setText(Mail);
+
+
+            Toast.makeText(InfoPage.this,"logged in",Toast.LENGTH_SHORT).show();
+        } else { // regular sign in
+
+          // gets last sign in
+            reference=FirebaseDatabase.getInstance().getReference().child("users");
+            Query query = reference.limitToLast(1);
+            query.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot child: dataSnapshot.getChildren()) {
+
+                       String passdb = child.child("password").getValue().toString();
+                       String userdb = child.child("username").getValue().toString();
+
+
+                        pass.setText(passdb);
+                        user.setText(userdb);
+                    }
+                }
+
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            Toast.makeText(InfoPage.this,"logged in",Toast.LENGTH_SHORT).show();
+
+
+
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), MainLoginPage.class));
+                    }
+                });
+            }
+        });
+    }
+    
+}
---
+++ app/src/main/java/com/example/tasker/login_regular.java
@@ -0,0 +1,121 @@
+package com.example.tasker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+public class login_regular extends AppCompatActivity {
+
+    EditText loginUsername, loginPassword;
+    TextView signupRedirectText;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_regular);
+
+
+        loginUsername = findViewById(R.id.signin_regular_username);
+        loginPassword = findViewById(R.id.signin_regular_password);
+        loginButton = findViewById(R.id.signin_regular_button);
+        signupRedirectText = findViewById(R.id.signin_regular_signUP);
+
+        loginButton.setOnClickListener(new View.OnClickListener() { // button action
+            @Override
+            public void onClick(View view) {
+
+                if (!validateUsername() | !validatePassword()) {
+
+                } else {
+                    checkUser();
+                }
+            }
+        });
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_regular.this, MainLoginPage.class));
+            }
+
+        });
+    }
+
+
+    public Boolean validateUsername() {
+        String val = loginUsername.getText().toString();
+        if (val.isEmpty()) {
+            loginUsername.setError("username cant be empty");
+            return false;
+        } else {
+            loginUsername.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword() {
+        String val = loginPassword.getText().toString();
+        if (val.isEmpty()) {
+            loginPassword.setError("password cant be empty");
+            return false;
+        } else {
+            loginPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser() {
+        String userUsername = loginUsername.getText().toString().trim();
+        String userPassword = loginPassword.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists()) {
+                    loginUsername.setError(null);
+                    String passwordFromDB = snapshot.child(userPassword).child("password").getValue(String.class);
+
+                    if (!Objects.equals(passwordFromDB, userPassword)) {
+                        loginUsername.setError(null);
+                        Intent intent = new Intent(login_regular.this, InfoPage.class);
+                        startActivity(intent);
+                        Toast.makeText(login_regular.this,"logged in",Toast.LENGTH_SHORT).show();
+                    } else {
+                        loginPassword.setError("invalid input");
+                        loginPassword.requestFocus();
+                    }
+                } else {
+                    loginUsername.setError("user does not exist");
+                    loginUsername.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
+
