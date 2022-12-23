ffc4e376cde391e924f38720f5a295f143f3a809
tomerklugman <tomerklu@gmail.com>
2022-12-23 20:26:23 +0200

    final signup+house add

---
+++ app/src/main/java/com/example/tasker/home_bottom_toolbar.java
@@ -0,0 +1,18 @@
+package com.example.tasker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tasker.databinding.ActivityMainLoginPageBinding;
+
+public class home_bottom_toolbar extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_bottom_toolbar);
+    }
+}
